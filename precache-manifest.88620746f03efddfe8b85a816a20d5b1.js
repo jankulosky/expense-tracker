7d6fb318e7cbd358111d445e15706b8f57da66d7
Viktor Jankuloski <viktor.jankuloski@hotmail.com>
2022-10-06 19:43:40 +0200

    Updates

---
+++ precache-manifest.88620746f03efddfe8b85a816a20d5b1.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "6d54d26ef174700645f7",
+    "url": "/expense-tracker/static/css/main.8c804024.chunk.css"
+  },
+  {
+    "revision": "6d54d26ef174700645f7",
+    "url": "/expense-tracker/static/js/main.6d54d26e.chunk.js"
+  },
+  {
+    "revision": "5eff2ae47f65703fbf85",
+    "url": "/expense-tracker/static/js/1.5eff2ae4.chunk.js"
+  },
+  {
+    "revision": "2c594b5b6bfedf8ec159",
+    "url": "/expense-tracker/static/js/runtime~main.2c594b5b.js"
+  },
+  {
+    "revision": "8ce365fe20903902e1dca2b348c81d67",
+    "url": "/expense-tracker/index.html"
+  }
+];
